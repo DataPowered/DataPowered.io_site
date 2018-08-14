f2a9de1efe6ddd08ad7c5189cc8506807640cdeb
CaterinaC <caterina.constantinescu@gmail.com>
2018-08-14 22:30:54 +0100

    Created 'hacky' event plot with randomly placed text labels for single events / day

--- site/content/Rscripts/2018-08-12-labelling-events-on-line-graphs.R
+++ site/content/Rscripts/2018-08-12-labelling-events-on-line-graphs.R
@@ -4,6 +4,8 @@
 
 library( data.table )
 
+library( stringr )
+
 library( plyr )
 library( tidyr )
 
@@ -60,16 +62,73 @@ views_time_events <- views_time_events[ Date > "2017-10-20", ]
 
 # Graphs ------------------------------------------------------------------
 
-ggplot( data = views_time_events, aes( x = Date, y = UniquePageViews )  ) +  
+
+# Step 1: fairly simple plot:
+base <- ggplot( data = views_time_events,
+                aes( x = Date, y = cumsum( UniquePageViews ) ) ) +  
   # geom_vline goes in first to be *underneath* the lines and points.
   geom_vline( xintercept = views_time_events[ ! is.na( Event_1 ), Date ],
-              linetype = 2, color = "red", alpha = 0.5, lwd = 0.75 ) +
-  geom_point( ) + 
-  geom_line( size = 1 ) +
-  ggtitle( "Daily unique views, relative to Git commits" ) +
-  ylab( "Unique views" ) +
+              linetype = 2, color = "navy", alpha = 0.35, lwd = 1 ) +
+  geom_point( size = 2 ) + 
+  geom_line( size = 1 )
+print( base )
+# But there are improvements to be made:
+
+
+
+better_labels <- base +
+  ggtitle( "Daily views over time, against labelled Git commits" ) +
+  ylab( "Views" ) +
   xlab( "Date" ) +
   scale_x_date( breaks = date_breaks( "months" ), labels = date_format( "%b-%y" ) )
+print( better_labels )
+
+
+# Adding text now:
+# set.seed( 1566604 )
+annotated_events <- better_labels + 
+  geom_label( aes( x = Date, y = 160 + rnorm( nrow( views_time_events ), 0, 25 ), 
+                   label = str_wrap( Event_1, width = 18 ) ), 
+              size = 3, angle = 90, color = "navy" )
+print( annotated_events )
+
+
+
+set.seed( 1566604 )
+# Why not also add total (non-unique) page views? Insert that UNDER all pre-existing layers:
+annotated_events$layers <- c( geom_point( aes( x = Date, y = cumsum( PageViews ) ), 
+                                          color = "red", alpha = 0.25,
+                                          size = 2 ),
+                              geom_line( aes( x = Date, y = cumsum( PageViews ) ),
+                                         color = "red", alpha = 0.25,
+                                         size = 1 ), 
+                              annotated_events$layers )
+print( annotated_events )
+
+
+
+
+annotated_events + 
+  annotate( "text", 
+            y = max( cumsum( views_time_events$UniquePageViews ) ), 
+            x = max( views_time_events$Date ) + 5,
+            label = str_wrap( "Unique page views", width = 7 ),
+            size = 3 ) +
+  annotate( "text", 
+            y = max( cumsum( views_time_events$PageViews ) ), 
+            x = max( views_time_events$Date ) + 5,
+            label = str_wrap( "Page views", width = 7 ),
+            size = 3,
+            color = "red" )
+
+
+
+
+# But what if we want to include all the other events too? i.e., multiple commits pushed on the same date. Would get messy...
+
+
+
+
 
 #   theme( axis.text.x = element_text( angle = 90, hjust = 1 ) ) +
 #   geom_label( aes( y = -2, x = 1, label = "Event ID:" ),
